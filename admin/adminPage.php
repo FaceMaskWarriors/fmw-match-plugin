38bda1f42a485953801193150b0066b777993f81
hifilorau <tomrau@gmail.com>
2020-05-06 14:42:41 -0400

    working admin react app set up at src/admin/index. Need to get hot loader working, currenlty have to kill front end every time we want to view js changes

--- admin/adminPage.php
+++ admin/adminPage.php
@@ -1,3 +1,3 @@
-<div class="optionsApp"></div>
+<div id="admin-app"></div>
 
 <script type="text/javascript" src="<?php echo plugin_dir_url(__FILE__)."/js/adminPage.js"?>"></script>
--- src/admin/index.js
+++ src/admin/index.js
@@ -1,1 +1,10 @@
 // Do Admin Page Code here... REACT?
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../containers/app';
+
+const rootEl = document.getElementById('admin-app');
+
+
+render(<App />, rootEl);
+
