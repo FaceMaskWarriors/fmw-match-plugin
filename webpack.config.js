5347aa3e32567ff5cb7bf9c36ae9bb05b256d343
hifilorau <tomrau@gmail.com>
2020-05-06 14:42:41 -0400

    working admin react app set up at src/admin/index. Need to get hot loader working, currenlty have to kill front end every time we want to view js changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,22 @@ module.exports = {
 //   entry: './src/index.js',
 	entry: {
 		'admin/js/adminPage': './src/admin/index.js',
-		// 'admin/js/admin' : './src/admin/index.js',
 	},
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname),
+	},
+	resolve: { extensions: ['*', '.js', '.jsx'] },
+	module: {
+		rules: [
+			{
+				test:/\.(js|jsx)$/,
+				exclude:/node_modules/,
+				loaders: ["react-hot-loader/webpack", "babel-loader"],
+				// use: {
+				// 		loader:'babel-loader',
+				// }
+			},
+		]
 	},
 };
